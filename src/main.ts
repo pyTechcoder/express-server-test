92fb1da49e19bdfb7bbb5e70f0f2fb150bc3f99e
Aman Singh <singhaman11415@gmail.com>
2024-04-30 15:50:12 +0530

    added express server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-import * as _ from 'lodash'
+import express from 'express'
+const app = express()
+const port = 3000
 
-console.log("hello world")
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
